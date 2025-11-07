add1c8b0a1a0fd2e0a018468dca97d3aea6e7146
Napraksin <nicolay@ivyflip.com>
2025-11-07 13:17:01 +0100

    github pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  // Use project site base in production (for GitHub Pages), and '/' in dev
+  base: mode === 'production' ? '/game-evo/' : '/',
   plugins: [react()],
-})
+}))
